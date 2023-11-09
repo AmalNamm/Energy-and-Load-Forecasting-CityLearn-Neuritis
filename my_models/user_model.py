086eb5b159848d65f1fed3f3cfeb44d124f1c9d0
phil_aupke <80556-phil_aupke@users.noreply.gitlab.aicrowd.com>
2023-11-09 15:45:01 +0000

    Changes for submission-fusion_2

--- my_models/user_model.py
+++ my_models/user_model.py
@@ -3,10 +3,13 @@ from my_models.example_predictor import ExamplePredictor
 from my_models.example_predictor2 import ExamplePredictor2
 from my_models.example_predictor_lstm import ExamplePredictorLSTM
 from my_models.example_predictor_LGB import ExamplePredictorLGB
+from my_models.example_predictor_fusion2 import ExamplePredictorFusion2
+
+
 
 ###################################################################
 #####                Specify your model here                  #####
 ###################################################################
 
 
-SubmissionModel = ExamplePredictorLGB
+SubmissionModel = ExamplePredictorFusion2
